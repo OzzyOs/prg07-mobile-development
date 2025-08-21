47364746b5071c4b637f39e4e807fdec10cc5d53
OzzyOs <joshuahursepuny@gmail.com>
2025-08-21 23:27:47 +0200

    Put color themes inside App.js so I covers the whole application.

--- pancake-finder/App.js
+++ pancake-finder/App.js
@@ -1,7 +1,25 @@
-import { NavigationContainer } from '@react-navigation/native';
+import { NavigationContainer, DefaultTheme, DarkTheme } from '@react-navigation/native';
 import MyTabs from './navtabs/MyTabs';
 
 export default function App() {
+
+  // Dit zijn de custom kleuren voor de "light theme".
+  const lightTheme = {
+    ...DefaultTheme,
+    color: {
+      primary: "#CCCCCC"
+    }
+  }
+  
+  // Dit zijn de custom kleuren voor de "dark theme".
+  const darkTheme = {
+    ...DarkTheme,
+    color: {
+      primary: "#1a1a1aff"
+    }
+  
+  }
+  
   return (
     <NavigationContainer>
       <MyTabs />
